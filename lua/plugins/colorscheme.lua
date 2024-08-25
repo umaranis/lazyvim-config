98df06f39ef74302c6ef8a925edcbab428c3f776
umaranis <mail@umaranis.com>
2024-08-25 15:33:47 +1000

    add colorschemes

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,10 @@
+return {
+  -- this theme doesn't appear in the telescope look
+  -- until applied through :colorscheme in the current session
+  -- maybe look at nightfox to see what it does to appear in telescope
+  -- :colorscheme catppuccin
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  -- :colorscheme nightfox
+  { "EdenEast/nightfox.nvim" },
+  { "rebelot/kanagawa.nvim" },
+}
