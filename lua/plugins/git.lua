f8dc8ca96b21c31aca90348f639883b333981cce
umaranis <mail@umaranis.com>
2024-08-25 15:35:32 +1000

    add git diffview plugin

---
+++ lua/plugins/git.lua
@@ -0,0 +1,9 @@
+return {
+  -- awesome git plugin, don't know why it is not more popular
+  -- VSCode like file or branch history
+  -- supports collapsing regions
+  -- https://github.com/sindrets/diffview.nvim?tab=readme-ov-file
+  {
+    "sindrets/diffview.nvim",
+  },
+}
