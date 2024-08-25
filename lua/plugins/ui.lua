faf6bc3c3aa51bca07e576b4fc8c8c95207d85c3
umaranis <mail@umaranis.com>
2024-08-25 16:40:07 +1000

    add color highlights

---
+++ lua/plugins/ui.lua
@@ -0,0 +1,14 @@
+return {
+  -- {
+  --   "NvChad/nvim-colorizer.lua",
+  --   config = function()
+  --     require("colorizer").setup({ filetypes = { "css", "html" }, user_default_options = { names = true } })
+  --   end,
+  -- },
+  {
+    "brenoprata10/nvim-highlight-colors",
+    config = function()
+      require("nvim-highlight-colors").setup()
+    end,
+  },
+}
