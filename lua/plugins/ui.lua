1157b7d93a09474dc55a80d5e32197d0c72f9e18
umaranis <mail@umaranis.com>
2024-08-25 16:54:28 +1000

    get rid of the warning for missing param

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -8,7 +8,7 @@ return {
   {
     "brenoprata10/nvim-highlight-colors",
     config = function()
-      require("nvim-highlight-colors").setup()
+      require("nvim-highlight-colors").setup({})
     end,
   },
 }
