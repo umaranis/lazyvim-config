73995b0044fa260ae38e362e4ad8d115844c6438
umaranis <mail@umaranis.com>
2024-09-04 18:57:38 +1000

    telescope - add fzf native, tabs picker and smart_open files picker

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,45 @@
+return {
+  -- provides better performance as a native implementation instead of lua
+  -- support fzf search syntax
+  {
+    "nvim-telescope/telescope-fzf-native.nvim",
+    build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+    config = function()
+      require("telescope").load_extension("fzf")
+    end,
+  },
+  --
+  -- search tabs using telescope
+  {
+    "LukasPietzschmann/telescope-tabs",
+    keys = {
+      { "<leader><tab>t", "<cmd>Telescope telescope-tabs list_tabs<cr>", desc = "List tabs" },
+    },
+    config = function()
+      require("telescope").load_extension("telescope-tabs")
+      require("telescope-tabs").setup({
+        -- Your custom config :^)
+      })
+    end,
+    dependencies = { "nvim-telescope/telescope.nvim" },
+  },
+  --
+  -- add a new picker called smart_open
+  {
+    "danielfalk/smart-open.nvim",
+    branch = "0.2.x",
+    config = function()
+      require("telescope").load_extension("smart_open")
+    end,
+    dependencies = {
+      "kkharji/sqlite.lua",
+      -- Only required if using match_algorithm fzf
+      -- { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      -- Optional.  If installed, native fzy will be used when match_algorithm is fzy
+      -- { "nvim-telescope/telescope-fzy-native.nvim" },
+    },
+    keys = {
+      { "<leader>fo", "<cmd>Telescope smart_open<cr>", desc = "Smart Open" },
+    },
+  },
+}
